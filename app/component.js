16abd73f5c0b4463ba5d00d83b3e14fb959241d4
Marcel Krause <marcel.krause@useeds.de>
2016-10-25 15:38:18 +0200

    Initial commit

---
+++ app/component.js
@@ -0,0 +1,10 @@
+require('purecss');
+
+module.exports = function() {
+  var element = document.createElement('h1');
+
+  element.className = 'pure-button';
+  element.innerHTML = 'Hello World';
+
+  return element;
+};
